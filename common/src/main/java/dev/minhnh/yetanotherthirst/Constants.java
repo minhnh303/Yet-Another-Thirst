47afd5286d7d022e593b2b86f80583b994848e1e
minhnh303 <nguyenminh30032001@gmail.com>
2026-06-06 11:55:57 +0700

    Init with template

---
+++ common/src/main/java/dev/minhnh/yetanotherthirst/Constants.java
@@ -0,0 +1,11 @@
+package dev.minhnh.yetanotherthirst;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "yet_another_thirst";
+	public static final String MOD_NAME = "Yet Another Thirst";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
